6e294b1116d65e811430f0cfeed61638043e127d
sabba <microlio@gmail.com>
2010-03-25 22:30:41 +0200

    patchin 0001-Refactor-login-Create-logout muutokset on suoritettu

--- milloinviimeksi/app/controllers/application_controller.rb
+++ milloinviimeksi/app/controllers/application_controller.rb
@@ -5,6 +5,15 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
+  before_filter :authenticate_required
+
+  def authenticate_required
+    unless session[:user_id]
+      redirect_to session_path
+      return false
+    end
+  end
+
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
--- milloinviimeksi/app/models/user.rb
+++ milloinviimeksi/app/models/user.rb
@@ -1,33 +1,47 @@
+require 'digest/sha2'
+
 class User < ActiveRecord::Base
 
   attr_accessor :password, :password_confirmation
 
+  validates_presence_of :name
+  validates_uniqueness_of :name
+
+  before_create :create_salt
   before_save :hash_password
+
+# u = User.new :name => 'kisu', :password => 'kisu'
+
+  def correct_password? pwd
+    return User.hash_plaintext_password(pwd, self.salt) == hashed_password
+  end
 
   private
 
-  def self.hash_plaintext_password pwd
-    (pwd.reverse)
+  def self.hash_plaintext_password pwd, salt
+    crypter = Digest::SHA2.new(512)
+    crypter << salt
+    crypter << pwd
+    crypter.to_s
+  end
+
+
+  def create_salt
+    self.salt = "#{Time.now.strftime('%s')}#{Thread.current.object_id}"
   end
 
   def hash_password
     return if self.password.blank?
-    self.hashed_password = User.hash_plaintext_password(self.password)
+    create_salt unless salt
+    self.hashed_password = User.hash_plaintext_password(self.password, self.salt)
   end
 
   def self.authenticate name, password
     user = User.find_by_name name
-
     unless user and user.correct_password? password
       raise ArgumentError.new "Authentication failed"
     end
-
     return user
 
   end
-
-  def correct_password? pwd
-    return User.hash_plaintext_password(pwd) == hashed_password
-  end
-
 end
