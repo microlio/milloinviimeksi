d7c8d3eb0666aec2a178fb0afccec8e3e7311b8d
Juho Kilpikoski <juho.kilpikoski@iki.fi>
2010-04-29 15:58:07 +0300

    Uusimmat metriikat. Saikuro toimii nyt suoraan merticsissä!

--- milloinviimeksi/tmp/metric_fu/output/reek.js
+++ milloinviimeksi/tmp/metric_fu/output/reek.js
@@ -6,14 +6,14 @@
       g.marker_font_size = "10px"
 
         g.title = 'Reek: code smells';
-        g.data('Control Couple', [1,1,,,1,1,1,,1])
+        g.data('Control Couple', [1,1,,,1,1,1,,1,1])
 g.data('ControlCouple', [,,1,1,,,,1])
-g.data('Duplication', [6,6,8,8,6,6,6,6,7])
-g.data('Irresponsible Module', [3,5,,,7,7,7,,7])
+g.data('Duplication', [6,6,8,8,6,6,6,6,7,7])
+g.data('Irresponsible Module', [3,5,,,7,7,7,,7,7])
 g.data('IrresponsibleModule', [,,7,7,,,,7])
-g.data('Long Method', [2,2,,,2,2,2,,2])
+g.data('Long Method', [2,2,,,2,2,2,,2,2])
 g.data('LongMethod', [,,2,2,,,,2])
 g.data('Uncommunicative Name', [,,,,2])
 
-        g.labels = {"5":"4/13","0":"3/27","6":"4/15","1":"3/29","7":"4/19","2":"4/7","8":"4/26","3":"4/8","4":"4/12"};
+        g.labels = {"5":"4/13","0":"3/27","6":"4/15","1":"3/29","7":"4/19","2":"4/7","8":"4/26","3":"4/8","9":"4/29","4":"4/12"};
         g.draw();
